b79b93e267cb41fa4eeb74c41749acc4883e3adb
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-02-21 10:37:06 -0400

    Move to new Cachengo infra

--- ci_scripts/push_manifest.sh
+++ ci_scripts/push_manifest.sh
@@ -1,4 +1,5 @@
 export IMAGE_TAG=$(cat VERSION)
+export DOCKER_CLI_EXPERIMENTAL=enabled
 
 docker manifest create --amend cachengo/xos-api-tester:$IMAGE_TAG cachengo/xos-api-tester-x86_64:$IMAGE_TAG cachengo/xos-api-tester-aarch64:$IMAGE_TAG
 docker manifest push cachengo/xos-api-tester:$IMAGE_TAG
